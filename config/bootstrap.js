93f89fbbe9eab483f9b637c46298da86b234564c
Joshua Chan <joshua@appdevdesigns.net>
2015-10-08 08:31:29 -0700

    * Moved controller logic that is not directly related to client requests into the service object * sendEmail() is not split into sendScheduledEmail() and send() + subscribed to events sent from ADCore.queue.publish() and EmailNotifications.hub.pubilsh() + trigger() handles email event triggers (WIP)

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -10,37 +10,37 @@
 
 module.exports = function (cb) {
 
-	// setup our EmailNotifications API:
-	EmailNotifications.____init(cb);
-	
-	var schedule = require('node-schedule');
-  Object.keys(sails.config.crontab).forEach(function(key) {
-    var val = sails.config.crontab[key];
-    schedule.scheduleJob(key, val);
-  });
+    // setup our EmailNotifications API:
+    EmailNotifications.____init(cb);
+  
+    var schedule = require('node-schedule');
+    Object.keys(sails.config.crontab).forEach(function(key) {
+        var val = sails.config.crontab[key];
+        schedule.scheduleJob(key, val);
+    });
 
-	/*
-	 * If you have additional setup steps, then it might be better to use:
-
-	async.series([
-
-		// step 1: call EmailNotifications.____init()
-		function(next) {
-			EmailNotifications.____init(next);
-		},
-
-		// step 2: call ......
-		function(next) {
-			......(next);
-		},
-
-		// repeat for each setup routine 
-
-	],function(err, results){
-	
-		cb(err);
-	})
-
-	 */
+    /*
+     * If you have additional setup steps, then it might be better to use:
+  
+    async.series([
+  
+      // step 1: call EmailNotifications.____init()
+      function(next) {
+        EmailNotifications.____init(next);
+      },
+  
+      // step 2: call ......
+      function(next) {
+        ......(next);
+      },
+  
+      // repeat for each setup routine 
+  
+    ],function(err, results){
+    
+      cb(err);
+    })
+  
+     */
 
 };
